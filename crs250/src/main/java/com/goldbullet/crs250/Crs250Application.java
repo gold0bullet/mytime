34a763094c96c2b75c96434fb0067cdc5bff2c60
gold0bullet <leginwrocks@gmail.com>
2017-09-04 21:05:49 +0200

    Init

---
+++ crs250/src/main/java/com/goldbullet/crs250/Crs250Application.java
@@ -0,0 +1,12 @@
+package com.goldbullet.crs250;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crs250Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crs250Application.class, args);
+	}
+}
---
+++ crs250/src/test/java/com/goldbullet/crs250/Crs250ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goldbullet.crs250;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Crs250ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
